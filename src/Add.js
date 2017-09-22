737585bc61701a9f15834cea160922c34bc10329
alexanderkozhevin <turboasm21@gmail.com>
2017-09-22 17:42:02 +0300

    added basic router

---
+++ src/Add.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+class Add extends Component {
+  constructor(props){
+
+    super(props);
+
+  }
+  render() {
+    return (
+      <div >
+        Add page
+      </div>
+    );
+  }
+}
+
+export default Add;
--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Route,
+  Link
+} from 'react-router-dom'
 
+import Item from './Item';
+import List from './List';
+import Add from './Add';
 
 
 class App extends Component {
@@ -11,32 +19,39 @@ class App extends Component {
 
     this.state = {
       list : [
-        {"name":"Ox, musk","number":"672-927-6623","avatar":"https://robohash.org/utdoloret.bmp?size=50x50&set=set1"},
-        {"name":"Vicuna","number":"644-749-9803","avatar":"https://robohash.org/mollitiaestharum.bmp?size=50x50&set=set1"},
-        {"name":"Australian sea lion","number":"156-330-4905","avatar":"https://robohash.org/nonquiaquaerat.bmp?size=50x50&set=set1"},
-        {"name":"Bear, grizzly","number":"210-398-5238","avatar":"https://robohash.org/nonnequeperspiciatis.bmp?size=50x50&set=set1"},
-        {"name":"Wallaroo, common","number":"211-865-6178","avatar":"https://robohash.org/eligendibeataeunde.png?size=50x50&set=set1"},
-        {"name":"Giant heron","number":"284-515-6756","avatar":"https://robohash.org/deseruntaccusantiumdolores.jpg?size=50x50&set=set1"},
-        {"name":"Hawk-headed parrot","number":"780-206-1559","avatar":"https://robohash.org/idutaccusamus.jpg?size=50x50&set=set1"},
-        {"name":"Killer whale","number":"524-678-5324","avatar":"https://robohash.org/consequatursedin.bmp?size=50x50&set=set1"},
-        {"name":"Langur, gray","number":"616-318-0351","avatar":"https://robohash.org/nobiseximpedit.bmp?size=50x50&set=set1"},
-        {"name":"Boa, malagasy ground","number":"361-139-7222","avatar":"https://robohash.org/etodiounde.jpg?size=50x50&set=set1"}
+        {"name":"Stork, white-necked","number":"964-750-1864","avatar":"https://robohash.org/numquamdolorelaboriosam.bmp?size=50x50&set=set1","id":"b0572828-dfb1-4f68-8c50-6c6ebd2a9379"},
+        {"name":"Small Indian mongoose","number":"637-891-6335","avatar":"https://robohash.org/exercitationeminciduntvoluptatem.jpg?size=50x50&set=set1","id":"fb216fba-b9af-4f6a-9eaa-c895b8a33450"},
+        {"name":"Eland, common","number":"861-846-7429","avatar":"https://robohash.org/uteteos.png?size=50x50&set=set1","id":"c406f924-7701-4c11-b30b-cfd62e10fcad"},
+        {"name":"Swallow-tail gull","number":"404-142-4453","avatar":"https://robohash.org/aliasmollitiaquibusdam.png?size=50x50&set=set1","id":"643225c2-ab39-4505-857a-0177d2d5b32c"},
+        {"name":"Denham's bustard","number":"900-390-4805","avatar":"https://robohash.org/enimvoluptatevitae.jpg?size=50x50&set=set1","id":"0d1d1309-9b01-4ce1-b0b6-f5f9d5daf80e"},
+        {"name":"Lion, southern sea","number":"875-374-2724","avatar":"https://robohash.org/animimagnilabore.png?size=50x50&set=set1","id":"f0cc2c9d-563c-4298-8f98-4dccc3d9d38e"},
+        {"name":"Phascogale, brush-tailed","number":"755-500-5716","avatar":"https://robohash.org/delectusnonet.jpg?size=50x50&set=set1","id":"14f3f6e5-eacf-415b-965a-5eae59212722"},
+        {"name":"Boubou, southern","number":"852-307-2229","avatar":"https://robohash.org/oditeosvoluptate.jpg?size=50x50&set=set1","id":"5defd205-d76f-451c-bada-50b029cc7f86"},
+        {"name":"Short-nosed bandicoot","number":"186-967-5236","avatar":"https://robohash.org/porroutasperiores.bmp?size=50x50&set=set1","id":"dbb10cf4-ce71-42d9-b11c-66cc31128ecf"},
+        {"name":"Kookaburra, laughing","number":"416-280-4732","avatar":"https://robohash.org/fugiatadipiscisaepe.jpg?size=50x50&set=set1","id":"31af77ce-2038-43f9-b598-baeea9f0bfc9"}
       ]
     }
   }
   render() {
     return (
       <div className="App">
-        {this.state.list.map((item, index)=>{
-          return(
-            <div key={index}>
-              <img src={item.avatar} />
-              <div >{item.name}</div>
-              <div >{item.number}</div>
-            </div>
+        <Router>
+          <div>
+            
+            <ul>
+              <li><Link to="/">List</Link></li>
+              <li><Link to="/add">Add</Link></li>
+              <li><Link to="/contact/2345">Item</Link></li>
+            </ul>
 
-          )
-        })}
+            <hr/>
+
+            <Route exact path="/" component={List}/>
+            <Route exact path="/add" component={Add}/>
+            <Route path="/contact/:id" component={Item}/>
+
+          </div>
+        </Router>
       </div>
     );
   }
