533d205e8e5a7de5bf922c5c20caadda58988e77
alexanderkozhevin <turboasm21@gmail.com>
2017-09-22 17:07:10 +0300

    started project

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+
+
+class App extends Component {
+  constuctor(props){
+
+  }
+  render() {
+    return (
+      <div className="App">
+
+      </div>
+    );
+  }
+}
+
+export default App;
